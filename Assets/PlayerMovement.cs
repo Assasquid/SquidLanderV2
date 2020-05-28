44780c59050e74f47bbbb538bd833fe94339f548
AssaSquid <jerome@nebula-tales.com>
2020-05-28 15:11:22 +0100

    First commit, setting up repos + game.
    First movements for the player.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float swimSpeed = 2000f;
+    [SerializeField] float rotationSpeed = 300f;
+
+    float horizontalInput = 0f;
+
+    Rigidbody2D rigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal") * rotationSpeed *Time.deltaTime;
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+
+        RespondToRotateInput();
+        RespondToSwimInput();
+    }
+
+    private void RespondToSwimInput()
+    {
+        if (Input.GetButton("Swim"))
+        {
+
+            if (Input.GetButton("Boost"))
+            {
+                ApplySwim(swimSpeed * 4);
+            }
+
+            else
+            {
+                ApplySwim(swimSpeed);
+            }
+        }
+    }
+
+    private void ApplySwim(float actualSpeed)
+    {
+        FreezeRotation(true);
+        float speedThisFrame = actualSpeed * Time.deltaTime;
+        rigidBody2D.AddRelativeForce(Vector3.up * speedThisFrame);
+        FreezeRotation(false);
+    }
+
+    private void RespondToRotateInput()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            RotateSquid(horizontalInput);
+        }
+    }
+
+    private void RotateSquid(float rotationThisFrame)
+    {
+        FreezeRotation(true);
+        transform.Rotate(Vector3.back * rotationThisFrame);
+        FreezeRotation(false);
+    }
+
+    private void FreezeRotation(bool isFrozen)
+    {
+        if (isFrozen)
+        {
+            rigidBody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+
+        else
+        {
+            rigidBody2D.constraints = RigidbodyConstraints2D.None;
+        }    
+    }
+
+}
